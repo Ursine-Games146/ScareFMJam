5f1eb529e47242ffc2c725c9c82a012dff77172b
Ursine-Games146 <trod146@gmail.com>
2020-12-04 17:50:59 -0500

    Prototype
    Initial

---
+++ Assets/Scripts/Environment/Door1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Door1 : MonoBehaviour
+{
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                SceneManager.LoadScene(1);
+            }
+        }
+    }
+}
