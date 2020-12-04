a4da66ccbc508ffc45b24422d117fce95fe79f53
Ursine-Games146 <trod146@gmail.com>
2020-12-04 17:39:04 -0500

    Movement
    Basic left and right movement. Very basic.

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+
+    Rigidbody2D rb2d;
+    
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        CheckMovement();
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+
+    void CheckMovement()
+    {
+        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb2d.velocity = Vector2.left * speed;
+        }
+        else if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            rb2d.velocity = Vector2.right * speed;
+        }
+        else
+        {
+            rb2d.velocity = Vector2.zero;
+        }
+    }
+}
