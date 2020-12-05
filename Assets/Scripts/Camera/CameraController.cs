0b39baf703897ef0b1941f78696a9975dd57bccd
Ursine-Games146 <trod146@gmail.com>
2020-12-04 19:42:48 -0500

    Camera
    Camera Controller script and settings.

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	public float speed = 15f;
+	public float minDistance;
+	public GameObject target;
+	public Vector3 offset;
+
+	private Vector3 targetPos;
+
+	// Use this for initialization
+	void Start()
+	{
+		targetPos = transform.position;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (target)
+		{
+			Vector3 posNoZ = transform.position + offset;
+			Vector3 targetDirection = (target.transform.position - posNoZ);
+			float interpVelocity = targetDirection.magnitude * speed;
+			targetPos = (transform.position) + (targetDirection.normalized * interpVelocity * Time.deltaTime);
+			transform.position = Vector3.Lerp(transform.position, targetPos, 0.25f);
+
+		}
+	}
+}
