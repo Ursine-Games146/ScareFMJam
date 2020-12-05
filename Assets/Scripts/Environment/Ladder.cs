3dda87eafe5669daab1ae0fb6066accb4f5ebb57
Ursine-Games146 <trod146@gmail.com>
2020-12-04 22:11:59 -0500

    2nd Prototype
    Stairs and Ladders

---
+++ Assets/Scripts/Environment/Ladder.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ladder : MonoBehaviour
+{
+
+    public float speed;
+
+    Rigidbody2D rb2d;
+    GameObject player;
+
+    void Start()
+    {
+        rb2d = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            rb2d.gravityScale = 0;
+
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+            {
+                rb2d.velocity = Vector2.up * speed;
+            }
+            else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+            {
+                rb2d.velocity = Vector2.down * speed;
+            }
+        }
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            rb2d.gravityScale = 1;
+        }
+    }
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed;
+    public float speed, jumpForce, groundRadius;
+    public float fallMultiple = 2.5f;
+    public float lowJump = 2f;
+
+    [SerializeField] private bool isGrounded;
 
     Rigidbody2D rb2d;
+
+    public Transform detectGround;
+    public LayerMask whatIsGround;
+
     
     void Start()
     {
@@ -17,6 +25,25 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         CheckMovement();
+        CheckJump();
+
+        Collider2D collider = Physics2D.OverlapCircle(detectGround.position, groundRadius, whatIsGround);
+        if(collider != null)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+
+        if(rb2d.gravityScale == 1 && !isGrounded)
+        {
+            if (rb2d.velocity.y < 0)
+            {
+                rb2d.velocity += Vector2.up * Physics2D.gravity * (fallMultiple - 1) * Time.deltaTime;
+            }
+        }
     }
 
     void FixedUpdate()
@@ -28,15 +55,29 @@ public class PlayerMovement : MonoBehaviour
     {
         if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-            rb2d.velocity = Vector2.left * speed;
+            rb2d.velocity = new Vector2(-1.0f * speed, rb2d.velocity.y);
         }
         else if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            rb2d.velocity = Vector2.right * speed;
+            rb2d.velocity = new Vector2(1.0f * speed, rb2d.velocity.y);
         }
-        else
+        else if(rb2d.velocity.y == 0)
         {
             rb2d.velocity = Vector2.zero;
         }
+        
+    }
+
+    void CheckJump()
+    {
+        if(Input.GetKey(KeyCode.Space) && isGrounded)
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpForce);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(detectGround.position, groundRadius);
     }
 }
